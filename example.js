371d284bfc542d47d8160a735342805cf2c34d19
deoxxa <deoxxa@fknsrs.biz>
2013-10-08 17:51:55 +1100

    make example use MediaType.whatever methods

--- example.js
+++ example.js
@@ -1,6 +1,4 @@
-var parseMedia = require('./contenttype').parseMedia;
-var select = require('./contenttype').select;
-var splitQuotedString = require('./contenttype').splitQuotedString;
+var MediaType = require('./contenttype');
 
 var representations = [
   'text/html',
@@ -12,10 +10,10 @@ var representations = [
   '*/*;q=0'
 ];
 
-var accept = splitQuotedString('application/json, application/json;profile="a,b;c.json?d=1;f=2";q=0.2 text/turtle, text/html;q=0.50, */*;q=0.01', ',');
+var accept = MediaType.splitQuotedString('application/json, application/json;profile="a,b;c.json?d=1;f=2";q=0.2 text/turtle, text/html;q=0.50, */*;q=0.01', ',');
 
-console.log('Formats:\n\t' + representations.map(parseMedia).join('\n\t'));
+console.log('Formats:\n\t' + representations.map(MediaType.parseMedia).join('\n\t'));
 
-console.log('Accept:\n\t' + accept.map(parseMedia).join('\n\t'));
+console.log('Accept:\n\t' + accept.map(MediaType.parseMedia).join('\n\t'));
 
-console.log('Selected:', (select(representations.map(parseMedia), accept.map(parseMedia)) || 'None').toString());
+console.log('Selected:', (MediaType.select(representations.map(MediaType.parseMedia), accept.map(MediaType.parseMedia)) || 'None').toString());
