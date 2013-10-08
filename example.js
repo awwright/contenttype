86d1d4dd107eaf29e1306e24ac16c186c3adfdfd
deoxxa <deoxxa@fknsrs.biz>
2013-10-08 18:25:40 +1100

    make example more readable

--- example.js
+++ example.js
@@ -1,16 +1,22 @@
 var MediaType = require('./contenttype');
 
 var representations = [
+  'application/json',
   'text/html',
-  'text/turtle',
-  'application/json;q=1;profile="schema.json?x=y"',
+  'application/json;profile="schema.json"',
+  'application/json;profile="different.json"',
   'application/json;q=1',
   'text/plain;q=0.2',
   'text/*;q=0.00002', // ABNF limits to three decimal places, this will toString() as "0"
   '*/*;q=0'
 ];
 
-var accept = MediaType.splitQuotedString('application/json, application/json;profile="a,b;c.json?d=1;f=2";q=0.2 text/turtle, text/html;q=0.50, */*;q=0.01', ',');
+var accept = [
+  'text/html;q=0.50',
+  '*/*;q=0.01',
+  'application/json;profile=different.json',
+  'application/json;profile="a,b;c.json?d=1;f=2";q=0.2',
+];
 
 console.log('Formats:\n\t' + representations.map(MediaType.parseMedia).join('\n\t'));
 
